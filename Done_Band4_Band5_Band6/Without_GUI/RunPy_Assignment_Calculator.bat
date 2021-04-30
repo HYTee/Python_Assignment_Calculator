d8378f726708a4154243c24cce37fd296a698877
HYTee <teehockyan@gmail.com>
2021-04-30 09:55:47 +0800

    30th Apr 2021 - Finalized with two versions such as with_GUI and without_GUI

---
+++ Done_Band4_Band5_Band6/Without_GUI/RunPy_Assignment_Calculator.bat
@@ -0,0 +1,9 @@
+@echo off
+cd C:\Users\teeho\Desktop\py\Assignment\Done_Band4_Band5_Band6\Without_GUI
+set root=C:\ProgramData\Miniconda3
+call %root%\Scripts\activate.bat %root%
+call activate learn-python
+test&cls
+call python Resistor.py
+pause
+
